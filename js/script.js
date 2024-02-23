a7f49e38fe0fc9f7b62d78f253ef054be3a3f60b
TimurBinder <111724672+TimurBinder@users.noreply.github.com>
2024-02-23 17:06:43 +0500

    update

--- js/script.js
+++ js/script.js
@@ -24,6 +24,45 @@ sliders.forEach(slider => {
     });
 });
 
+let navLine = document.querySelector('.liaison__body hr.active');
+let navItems = document.querySelectorAll('.liaison__body .menu__item');
+
+navItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        switch(index) {
+            case 0:
+                navLine.style.left = `0%`;
+                break;
+
+            case 1:
+                navLine.style.left = `14%`;
+                break;
+
+            case 2:
+                navLine.style.left = `28%`;
+                break;
+
+            case 3:
+                navLine.style.left = `45%`;
+                break;
+
+            case 4:
+                navLine.style.left = `60%`;
+                break;
+
+            case 5:
+                navLine.style.left = `70%`;
+                break;
+
+            case 6:
+                navLine.style.left = `82%`;
+                break;
+        }
+    });
+});
+
 let socialsBtn = document.querySelector('.block__bag');
 
-// socialsBtn.addEventListener('click', )
+socialsBtn.addEventListener('click', () => {
+    document.querySelector('.networks__block').classList.toggle('active');
+});
